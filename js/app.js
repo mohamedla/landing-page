a1bc97756f7fc301e3e672b4b67a5f6bebe95061
Mohamed Ashraf Tolba <mohamed.ashraf.memo@gmail.com>
2022-01-14 03:43:07 +0200

    make the first section and its link active by default, 'functionality' the section and its link in viewport have class active

--- js/app.js
+++ js/app.js
@@ -1,47 +1,47 @@
 /**
  * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
+ * Manipulating the DOM exercise. ✓
+ * Exercise programmatically builds navigation, ✓
+ * scrolls to anchors from navigation, ✓
+ * and highlights section in viewport upon scrolling. ✓
  * 
- * Dependencies: None
+ * Dependencies: None ✓
  * 
- * JS Version: ES2015/ES6
+ * JS Version: ES2015/ES6 ✓
  * 
- * JS Standard: ESlint
+ * JS Standard: ESlint ✓
  * 
 */
 
 /**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
+ * Comments should be present at the beginning of each procedure and class. ✓
+ * Great to have comments before crucial code sections within the procedure. ✓
 */
 
 /**
- * Define Global Variables
+ * Define Global Variables ✓
  * 
 */
 
 
 /**
- * End Global Variables
- * Start Helper Functions
+ * End Global Variables ✓
+ * Start Helper Functions ✓
  * 
 */
 
 
 
 /**
- * End Helper Functions
- * Begin Main Functions
+ * End Helper Functions ✓
+ * Begin Main Functions ✓
  * 
 */
 
-// build the nav
+// build the nav  ✓
 
 
-// Add class 'active' to section when near top of viewport
+// Add class 'active' to section when near top of viewport  ✓
 
 
 // Scroll to anchor ID using scrollTO event
@@ -52,23 +52,95 @@
  * Begin Events
  * 
 */
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-const navMenu = document.querySelector('.navbar__menu');
-const navBar = document.createElement('ul');
-const sections = document.querySelectorAll('section');
-for (const section of sections) {
-    const title = section.querySelector('h2');
-    const newSpan = document.createElement('li');
-    newSpan.textContent = title.innerText;
-    newSpan.addEventListener('click',()=>{
-        section.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-    });
-    navBar.append(newSpan);
+/* build link menu by taking 
+    navigation->the section that will hold menu containercontainer
+    sectionList->the list of sections that need to pe linked in menu
+    container->container that hold links and will be appended by navigation
+*/
+function buildMenu(navigation,sectionList,container) {
+    for (const section of sectionList) {
+        const newSpan = document.createElement('li');
+        newSpan.textContent = section.getAttribute('data-nav');
+        newSpan.setAttribute('id',section.getAttribute('id')+'_link')
+        // Scroll to section on link click
+        newSpan.addEventListener('click',()=>{
+            section.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+        });
+        container.append(newSpan);
+    }
+    navigation.append(container);
 }
-navMenu.append(navBar);
+
+// Global variables for multi use
+const navMenu = document.querySelector('.navbar__menu');
+const sections = document.querySelectorAll('section');
+const navBar = document.createElement('ul');
+
+// Build menu
+buildMenu(navMenu,sections,navBar);
+
+// making the frist section link active by default
+const navItems = navBar.querySelectorAll('li'); //global var but need to declar after building list
+navItems[0].classList.add('active');
+
+// control the menu diplay in small width
+    // function to toggle between 2 values to the same property
+    function toggleCssProp(element,prop,style1,style2) {
+        element.style[prop] = (element.style[prop] === style1)?style2:style1;
+    }
+// make the menuTriger toggle the navBar between viewed and hidden on click
+const menuTriger = document.querySelector('svg.menu_icon');
+menuTriger.addEventListener('click',()=>{
+    toggleCssProp(navBar,'display','block','none');
+});
+
+// on resize the page the nav menu cuz the menu triger may case some conflict with the style we want to diaplay
+window.addEventListener('resize',()=>{
+    if (window.innerWidth > 480) {
+        navBar.style.display = 'block';
+    } else {
+        navBar.style.display = 'none';
+    }
+});
+
+// determine the closest section of the sectionList to the top of the viewport
+function topClosest(sectionList) {
+    let theClosest;
+    let topDistance;
+    for (const section of sectionList) {
+        // the value still have no value
+        if (topDistance == undefined) {
+            theClosest = section;
+            topDistance = section.getBoundingClientRect().top;
+        }else if((topDistance >= 0 && section.getBoundingClientRect().top >= 0) && (section.getBoundingClientRect().top < topDistance)){
+        // the 2 values are positive && the section closer to top that the one register in theClosest
+            theClosest = section;
+            topDistance = section.getBoundingClientRect().top;
+        }else if(topDistance < 0 && (-(topDistance) >= ((section.getBoundingClientRect().height)*2.5/3))){
+            // the value in the topDistance is nigative && the topDistance greater than the hieght mean that the section is out in viewport 
+            // (note 2.5/3 of the height represent the content height as height represent the (content height + boder + padding))
+            theClosest = section;
+            topDistance = section.getBoundingClientRect().top;
+        }
+    }
+    return theClosest;
+}
+// make the given theSection and its link active and unactivate the other section and its links in  sectionList linkList
+function makeActive(theSection, sectionList, linkList) {
+    if (!theSection.classList.contains('active')) {
+        // remove active class from all section
+        for (const section of sectionList) {
+            section.classList.remove('active');
+        }
+        for (const item of linkList) {
+            item.classList.remove('active');
+        }
+        // add active class to viewport section and its link
+        theSection.classList.add('active');
+        navBar.querySelector('li#'+theSection.getAttribute('id')+'_link').classList.add('active');
+    }
+}
+// Set the closest to top sections as active on scrolling
+window.addEventListener('scroll',()=>{
+    makeActive(topClosest(sections),sections,navItems);
+});
